e14a43f8c7ada21e0d5927c853fdbc2245c9f6fa
Matt Engledowl <mengledowl@gmail.com>
2017-08-06 19:41:03 -0500

    Add shortBody to PostType

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -5,4 +5,9 @@ Types::PostType = GraphQL::ObjectType.define do
   field :id, !types.ID, 'The ID of the blog post'
   field :title, types.String, 'A user-friendly title for the blog post'
   field :body, types.String, 'The main body of content for the blog post'
+  field :shortBody, types.String, 'A shortened version of the body' do
+    resolve ->(obj, args, ctx) {
+      obj.body[0, 5]
+    }
+  end
 end
